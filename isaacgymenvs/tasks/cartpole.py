02207eb3dd6b9d706ac539aa2a4db01565323c73
Naoki04 <naoki.shitanda.phone@gmail.com>
2024-12-26 02:20:12 +0900

    jedyでのゴミ収集タスク用に、ひとまず球1つとjedyをpopできるようにした。

--- isaacgymenvs/tasks/cartpole.py
+++ isaacgymenvs/tasks/cartpole.py
@@ -52,6 +52,12 @@ class Cartpole(VecTask):
         self.dof_state = gymtorch.wrap_tensor(dof_state_tensor)
         self.dof_pos = self.dof_state.view(self.num_envs, self.num_dof, 2)[..., 0]
         self.dof_vel = self.dof_state.view(self.num_envs, self.num_dof, 2)[..., 1]
+        
+        actor_root_state_tensor = self.gym.acquire_actor_root_state_tensor(self.sim)
+        self._actors_root_state = gymtorch.wrap_tensor(actor_root_state_tensor).view(-1, 13)
+        
+        
+        
 
     def create_sim(self):
         # set the up axis to be z-up given that assets are y-up by default
@@ -68,6 +74,15 @@ class Cartpole(VecTask):
         self.gym.add_ground(self.sim, plane_params)
 
     def _create_envs(self, num_envs, spacing, num_per_row):
+        
+        prim_names = ["robot", "sphere"]
+        self.gym_assets = dict.fromkeys(prim_names)
+        self.gym_indices = dict.fromkeys(prim_names)
+        self.gym_indices["robot"] = []
+        self.gym_indices["sphere"] = []
+        
+        
+        
         # define plane on which environments are initialized
         lower = gymapi.Vec3(0.5 * -spacing, -spacing, 0.0) if self.up_axis == 'z' else gymapi.Vec3(0.5 * -spacing, 0.0, -spacing)
         upper = gymapi.Vec3(0.5 * spacing, spacing, spacing)
@@ -84,9 +99,11 @@ class Cartpole(VecTask):
         asset_file = os.path.basename(asset_path)
 
         asset_options = gymapi.AssetOptions()
-        asset_options.fix_base_link = True
-        cartpole_asset = self.gym.load_asset(self.sim, asset_root, asset_file, asset_options)
-        self.num_dof = self.gym.get_asset_dof_count(cartpole_asset)
+        asset_options.fix_base_link = True # True
+        asset_options.flip_visual_attachments = True # True
+        #cartpole_asset = self.gym.load_asset(self.sim, asset_root, asset_file, asset_options)
+        self.gym_assets["robot"] = self.gym.load_asset(self.sim, asset_root, asset_file, asset_options)
+        self.num_dof = self.gym.get_asset_dof_count(self.gym_assets["robot"])
 
         pose = gymapi.Transform()
         if self.up_axis == 'z':
@@ -96,25 +113,49 @@ class Cartpole(VecTask):
         else:
             pose.p.y = 2.0
             pose.r = gymapi.Quat(-np.sqrt(2)/2, 0.0, 0.0, np.sqrt(2)/2)
+            
+        # sphereのassetを作成
+        self.ball_radius = 0.1
+        ball_options = gymapi.AssetOptions()
+        ball_options.density = 200
+        self.gym_assets["sphere"] = self.gym.create_sphere(self.sim, self.ball_radius, ball_options)
 
         self.cartpole_handles = []
+        self.sphere_handles = []
         self.envs = []
         for i in range(self.num_envs):
             # create env instance
             env_ptr = self.gym.create_env(
                 self.sim, lower, upper, num_per_row
             )
-            cartpole_handle = self.gym.create_actor(env_ptr, cartpole_asset, pose, "cartpole", i, 1, 0)
+            cartpole_handle = self.gym.create_actor(env_ptr, self.gym_assets["robot"], pose, "cartpole", i, 1, 0)
 
             dof_props = self.gym.get_actor_dof_properties(env_ptr, cartpole_handle)
             dof_props['driveMode'][0] = gymapi.DOF_MODE_EFFORT
             dof_props['driveMode'][1] = gymapi.DOF_MODE_NONE
             dof_props['stiffness'][:] = 0.0
             dof_props['damping'][:] = 0.0
-            self.gym.set_actor_dof_properties(env_ptr, cartpole_handle, dof_props)
-
+            self.gym.set_actor_dof_properties(env_ptr, cartpole_handle, dof_props)            
+            
+            
+            ball_pose = gymapi.Transform()
+            ball_pose.p.x = 0.2
+            ball_pose.p.z = 2.0
+            ball_handle = self.gym.create_actor(env_ptr, self.gym_assets["sphere"], ball_pose, "ball", i, 0, 0)
+            
+            cartpole_idx = self.gym.get_actor_index(env_ptr, cartpole_handle, gymapi.DOMAIN_SIM)
+            self.gym_indices["robot"].append(cartpole_idx)
+            sphere_idx = self.gym.get_actor_index(env_ptr, ball_handle, gymapi.DOMAIN_SIM)
+            self.gym_indices["sphere"].append(sphere_idx)
+            
             self.envs.append(env_ptr)
-            self.cartpole_handles.append(cartpole_handle)
+            
+            # convert gym indices from list to tensor
+        for asset_name, asset_indices in self.gym_indices.items():
+            self.gym_indices[asset_name] = torch.tensor(asset_indices, dtype=torch.long, device=self.device)
+            
+        
+        
 
     def compute_reward(self):
         # retrieve environment observations from buffer
@@ -142,20 +183,65 @@ class Cartpole(VecTask):
         return self.obs_buf
 
     def reset_idx(self, env_ids):
+        
+        self.reset_buf[env_ids] = 0
+        self.progress_buf[env_ids] = 0
+        
+        # ロボットのDOFの初期状態を設定
         positions = 0.2 * (torch.rand((len(env_ids), self.num_dof), device=self.device) - 0.5)
         velocities = 0.5 * (torch.rand((len(env_ids), self.num_dof), device=self.device) - 0.5)
+        
+        # ロボットのroot_stateの(初期座標・回転・速度・角速度)を設定
+        robot_positions = 0.2 * (torch.rand((len(env_ids), 3), device=self.device) - 0.5)
+        robot_positions[:, 2] = 1.0
+        robot_rotations = torch.zeros((len(env_ids), 4), device=self.device)
+        robot_rotations[:, 3] = 1.0
+        robot_velocities = torch.zeros((len(env_ids), 3), device=self.device)
+        robot_angular_velocities = torch.zeros((len(env_ids), 3), device=self.device)
+        
+        robot_root_state = torch.cat([robot_positions, robot_rotations, robot_velocities, robot_angular_velocities], dim=1) # (envs, 13)
+        # 球のroot_state(初期座標・回転・速度・角速度)を設定
+        sphere_positions = 0.2 * (torch.rand((len(env_ids), 3), device=self.device) - 0.5)*1.5
+        sphere_positions[:, 2] = 1.0
+        sphere_rotations = torch.zeros((len(env_ids), 4), device=self.device)
+        sphere_rotations[:, 3] = 1.0
+        sphere_velocities = torch.zeros((len(env_ids), 3), device=self.device)
+        sphere_angular_velocities = torch.zeros((len(env_ids), 3), device=self.device)
+        
+        sphere_root_state = torch.cat([sphere_positions, sphere_rotations, sphere_velocities, sphere_angular_velocities], dim=1) # (envs, 13)
+        # すべての初期状態を結合して、_actors_root_stateに設定 (envs*2, 13)
+        self._actors_root_state[env_ids, :] = robot_root_state
+        self._actors_root_state[len(env_ids)+env_ids, :] = sphere_root_state
+       
+        
 
         self.dof_pos[env_ids, :] = positions[:]
         self.dof_vel[env_ids, :] = velocities[:]
-
-        env_ids_int32 = env_ids.to(dtype=torch.int32)
-        self.gym.set_dof_state_tensor_indexed(self.sim,
-                                              gymtorch.unwrap_tensor(self.dof_state),
-                                              gymtorch.unwrap_tensor(env_ids_int32), len(env_ids_int32))
-
-        self.reset_buf[env_ids] = 0
-        self.progress_buf[env_ids] = 0
-
+        
+        
+        # インデックスを作成(前半がロボット、後半が球)
+        robot_indices = self.gym_indices["robot"][env_ids].to(torch.int32)
+        sphere_indices = self.gym_indices["sphere"][env_ids].to(torch.int32)
+        all_indices = torch.unique(torch.cat([robot_indices, sphere_indices]))
+        
+        # ロボットのDOFの初期状態を反映
+        self.gym.set_dof_state_tensor_indexed(
+                                            self.sim, 
+                                            gymtorch.unwrap_tensor(self.dof_state),
+                                            gymtorch.unwrap_tensor(robot_indices), 
+                                            len(robot_indices)
+                                            )
+        # ロボット・球のroot_stateの初期状態を反映
+        self.gym.set_actor_root_state_tensor_indexed(
+                                            self.sim,
+                                            gymtorch.unwrap_tensor(self._actors_root_state),
+                                            gymtorch.unwrap_tensor(all_indices),
+                                            len(all_indices)
+                                        )
+                                        
+        
+        
+        
     def pre_physics_step(self, actions):
         actions_tensor = torch.zeros(self.num_envs * self.num_dof, device=self.device, dtype=torch.float)
         actions_tensor[::self.num_dof] = actions.to(self.device).squeeze() * self.max_push_effort
@@ -189,8 +275,10 @@ def compute_cartpole_reward(pole_angle, pole_vel, cart_vel, cart_pos,
     reward = torch.where(torch.abs(cart_pos) > reset_dist, torch.ones_like(reward) * -2.0, reward)
     reward = torch.where(torch.abs(pole_angle) > np.pi / 2, torch.ones_like(reward) * -2.0, reward)
 
-    reset = torch.where(torch.abs(cart_pos) > reset_dist, torch.ones_like(reset_buf), reset_buf)
-    reset = torch.where(torch.abs(pole_angle) > np.pi / 2, torch.ones_like(reset_buf), reset)
-    reset = torch.where(progress_buf >= max_episode_length - 1, torch.ones_like(reset_buf), reset)
+    #reset = torch.where(torch.abs(cart_pos) > reset_dist, torch.ones_like(reset_buf), reset_buf)
+    #reset = torch.where(torch.abs(pole_angle) > np.pi / 2, torch.ones_like(reset_buf), reset)
+    #reset = torch.where(progress_buf >= max_episode_length - 1, torch.ones_like(reset_buf), reset)
+    # とりあえず、最大エピソード長に達したらリセットするようにする
+    reset = torch.where(progress_buf >= max_episode_length - 1, torch.ones_like(reset_buf), reset_buf)
 
     return reward, reset
